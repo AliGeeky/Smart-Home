712db8ec7fbd12046d98d38cd1fbecbc4c3fa0f7
AliGeeKy <dotnetdev071@gmail.com>
2024-06-15 15:11:51 +0430

    DHT11

---
+++ SmartHome.ino
@@ -0,0 +1,34 @@
+#include<Arduino>
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
+}
+
+void loop()
+{
+    delay(2000);
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t))
+    {
+        Serial.println("Error!!");
+        return;
+    }
+
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temp: ");
+    Serial.print(t);
+    Serial.println(" *C ");
+}
