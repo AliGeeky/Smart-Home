074dc61fa68d11ddea93fcd3bfde387024c90327
AliGeeKy <dotnetdev071@gmail.com>
2024-06-16 10:56:25 +0430

    LED Control with Light Sensor

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -1,5 +1,7 @@
-#include<Arduino>
+//#include<Arduino>
 #include "DHT.h"
+int ldrPin = A0;  // َAnalog pin
+int ldrValue = 0; // متغیری برای ذخیره مقدار خوانده شده از فتوسل
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -10,12 +12,12 @@ void setup()
 {
     Serial.begin(9600);
     dht.begin();
+    pinMode(13, HIGH);
 }
 
 void loop()
 {
     delay(2000);
-
     float h = dht.readHumidity();
     float t = dht.readTemperature();
 
@@ -31,4 +33,18 @@ void loop()
     Serial.print("Temp: ");
     Serial.print(t);
     Serial.println(" *C ");
+    //
+    ldrValue = analogRead(ldrPin); 
+    Serial.print("LDR Value: ");   
+    if(ldrValue >50){
+      Serial.println(" lights on");
+      digitalWrite(13, HIGH);
+    }
+    else{
+      Serial.println(" lights Off ");
+      digitalWrite(13, LOW);
+    }
+    Serial.println(ldrValue);
+    delay(500);
+    
 }
